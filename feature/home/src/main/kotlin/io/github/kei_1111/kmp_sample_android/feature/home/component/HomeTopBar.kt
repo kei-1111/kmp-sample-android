1cdb09618ac868885538eda5dd31d516685c5521
kei-1111 <kei_02_1111@icloud.com>
2025-10-25 15:32:11 +0900

    chore: TopBarを指の動きに合わせてTopBarが動くように変更

--- feature/home/src/main/kotlin/io/github/kei_1111/kmp_sample_android/feature/home/component/HomeTopBar.kt
+++ feature/home/src/main/kotlin/io/github/kei_1111/kmp_sample_android/feature/home/component/HomeTopBar.kt
@@ -1,20 +1,33 @@
 package io.github.kei_1111.kmp_sample_android.feature.home.component
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeTopBar(
+    scrollBehavior: TopAppBarScrollBehavior,
     modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior? = null,
 ) {
+    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+    val topPadding = lerp(
+        start = statusBarHeight,
+        stop = 0.dp,
+        fraction = scrollBehavior.state.collapsedFraction
+    )
+
     TopAppBar(
         title = {
             Text(
@@ -23,6 +36,7 @@ fun HomeTopBar(
             )
         },
         modifier = modifier,
+        windowInsets = WindowInsets(0.dp, topPadding, 0.dp, 0.dp),
         scrollBehavior = scrollBehavior,
     )
 }
@@ -31,5 +45,7 @@ fun HomeTopBar(
 @Composable
 @Preview
 private fun HomeTopBarPreview() {
-    HomeTopBar()
+    HomeTopBar(
+        scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+    )
 }
