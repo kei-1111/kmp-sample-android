ba5fa0e464e513e7c1935c772c6b7b0dfa8d7029
kei-1111 <kei_02_1111@icloud.com>
2025-10-25 18:22:04 +0900

    chore: Type情報が抜けていたため追加

--- feature/home/src/main/kotlin/io/github/kei_1111/kmp_sample_android/feature/home/component/MarsPropertyCard.kt
+++ feature/home/src/main/kotlin/io/github/kei_1111/kmp_sample_android/feature/home/component/MarsPropertyCard.kt
@@ -1,18 +1,23 @@
 package io.github.kei_1111.kmp_sample_android.feature.home.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -30,14 +35,38 @@ fun MarsPropertyCard(
         modifier = modifier.wrapContentSize(),
     ) {
         Column {
-            AsyncImage(
-                model = marsProperty.imgSrc,
-                contentDescription = null,
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(1f),
-                contentScale = ContentScale.Crop
-            )
+                    .aspectRatio(1f)
+            ) {
+                AsyncImage(
+                    model = marsProperty.imgSrc,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+
+                Text(
+                    text = marsProperty.type.name,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .background(
+                            color = MaterialTheme.colorScheme.primaryContainer,
+                            shape = RoundedCornerShape(
+                                topStart = 0.dp,
+                                topEnd = 0.dp,
+                                bottomStart = 12.dp,
+                                bottomEnd = 0.dp,
+                            )
+                        )
+                        .padding(horizontal = 16.dp)
+                        .padding(vertical = 4.dp),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    textAlign = TextAlign.Center
+                )
+            }
             Column(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
@@ -62,6 +91,7 @@ private fun MarsPropertyCardPreview() {
             imgSrc = "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/03900/opgs/edr/fcam/FLB_645486870EDR_F0481570FHAZ00323M_.JPG",
             price = 450000,
             type = PropertyType.RENT,
-        )
+        ),
+        modifier = Modifier.width(150.dp)
     )
 }
