b35ce3b05e72a58061abe83d5bc083dea8d3536b
kei-1111 <kei_02_1111@icloud.com>
2025-10-25 16:08:56 +0900

    chore: MarsPropertyCardを使用するように変更

--- feature/home/src/main/kotlin/io/github/kei_1111/kmp_sample_android/feature/home/HomeScreenContent.kt
+++ feature/home/src/main/kotlin/io/github/kei_1111/kmp_sample_android/feature/home/HomeScreenContent.kt
@@ -1,16 +1,23 @@
 package io.github.kei_1111.kmp_sample_android.feature.home
 
+import android.R.attr.bottom
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import io.github.kei_1111.kmp_sample_android.feature.home.component.MarsPropertyCard
 import io.github.kei_1111.kmp_sample_library.feature.home.HomeAction
 import io.github.kei_1111.kmp_sample_library.feature.home.HomeState
 
@@ -20,16 +27,22 @@ fun HomeScreenContent(
     onAction: (HomeAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
+    val navigationBarHeight = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+
+    LazyVerticalGrid (
+        columns = GridCells.Adaptive(150.dp),
         modifier = modifier,
-        contentPadding = WindowInsets.navigationBars.asPaddingValues()
+        contentPadding = PaddingValues(
+            start = 16.dp,
+            end = 16.dp,
+            bottom = navigationBarHeight
+        ),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         items(state.marsProperties) { marsProperty ->
-            AsyncImage(
-                model = marsProperty.imgSrc,
-                contentDescription = null,
-                modifier = Modifier.size(200.dp),
-                contentScale = ContentScale.Crop
+            MarsPropertyCard(
+                marsProperty = marsProperty,
             )
         }
     }
