3deca55f01a570747c5d9c03016986faafb66059
kei-1111 <kei_02_1111@icloud.com>
2025-10-26 19:46:25 +0900

    chore: GitHub Packagesでライブラリを管理するように変更したためを読み込むための設定を追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import kotlin.apply
+
 pluginManagement {
     repositories {
         google {
@@ -11,15 +14,30 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+val localProperties = Properties().apply {
+    val localPropertiesFile = File(rootDir, "local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { load(it) }
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kei-1111/kmp-sample-library")
+            credentials {
+                username = localProperties.getProperty("gpr.user") ?: System.getenv("GITHUB_ACTOR")
+                password = localProperties.getProperty("gpr.token") ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
-
-includeBuild("libs/kmp-sample-library")
 
 rootProject.name = "kmp-sample-android"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
