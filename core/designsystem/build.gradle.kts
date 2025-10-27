f677a6cdb7e60701b6f5fe86f1ec3496c56c4744
kei-1111 <kei_02_1111@icloud.com>
2025-10-27 12:49:13 +0900

    chore: :core:designsystemに作成したLibraryComposePluginを適応

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,34 +1,11 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kmp.sample.android.library.compose)
 }
 
 android {
     namespace = "io.github.kei_1111.kmp_sample_android.core.designsystem"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    debugImplementation(libs.androidx.ui.tooling)
 }
