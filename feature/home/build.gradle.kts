ef170412da985b052ac9024d603b71015bdf7585
kei-1111 <kei_02_1111@icloud.com>
2025-10-27 12:45:29 +0900

    chore: :featureにFeaturePluginを作成

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,60 +1,19 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kmp.sample.android.feature)
 }
 
 android {
     namespace = "io.github.kei_1111.kmp_sample_android.feature.home"
-    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         buildConfigField("String", "DRAWABLE_PATH", "\"${projectDir}/src/main/res/drawable\"")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
+    buildFeatures.buildConfig = true
 }
 
 dependencies {
-    implementation(projects.core.designsystem)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.coil.compose)
     implementation(libs.coil.network)
-    implementation(libs.koin.android)
-    implementation(libs.koin.compose)
-    implementation(libs.koin.compose.viewmodel)
     implementation(libs.kmp.sample.library.home)
-    debugImplementation(libs.androidx.ui.tooling)
 }
